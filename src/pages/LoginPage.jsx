29850b92ee1caa1a466a6a792e936ff47bc2823a
Ravindra Balaji nagula <118665047+Ravindra-balaji@users.noreply.github.com>
2025-07-18 13:40:00 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { axiosInstance } from "../axios/axiosInstance";
 import { ErrorToast, SuccessToast } from "../utils/toastHelper";
 
